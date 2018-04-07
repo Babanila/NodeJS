d1a5887f8589843b4f69fa2f6ef82d7e1db29a2f
Babajide Williams <babanila@yahoo.com>
2018-04-07 07:31:50 +0200

    k

--- NodeJS-Codes/emailSending.js
+++ NodeJS-Codes/emailSending.js
@@ -1,56 +1,54 @@
 
-
+// Direct Email sending without security
 var nodemailer = require('nodemailer');
 var nconf = require('nconf');
 
-nconf.file({file: 'config.json'}).env();
-//nconf.argv().file({file: __dirname + 'config.json'});
-
-var transporter = nodemailer.createTransport({
-        service: 'gmail',
-            auth: {
-                user: nconf.get('GMAIL_USER'),
-                pass: nconf.get('GMAIL_PASS')
-            }
-
-    });
-
-var mailOptions = {
-        from: nconf.get('SENDER'),
-        to: nconf.get('RECEIVER'),
-        subject: 'Using Node.js to send email',
-        text: 'Programming is sweet to learn, trying out new things bcos my God is able'
-};
-
-
-transporter.sendMail(mailOptions, function(err, data){
-        if (err){
-            console.log('Error, check your stuff');
-        }
-        else{
-            console.log('Email Sent: ' + data.response);
-        }
-});
-
-/*
-// Direct Email sending without security
-var nodemailer = require('nodemailer');
+nconf.argv().file({file: __dirname + '/config.json'});
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'xxxxxxxx@gmail.com',
-        pass: 'xxxxxxxxxxx'
+        user: nconf.get('GMAIL_USER'),
+        pass: nconf.get('GMAIL_PASS')
     }
 });
 
-var mailOptions = {
-    from: 'xxxxxxxxx@gmail.com',
-    to: 'xxxxxxxx@yahoo.com',
+var mailDetails = {
+    from: nconf.get('GMAIL_USER'),
+    to: nconf.get('RECEIVER'),
     subject: 'Using Node.js to send email',
     text: 'Trying out new things bcos my God is able. Programming is sweet '
 };
 
-transporter.sendMail(mailOptions, function(err, data){
+transporter.sendMail(mailDetails, function(err, data){
+    if (err){
+        console.log('Error, check your stuff');
+    }
+    else{
+        console.log('Email Sent: ' + data.response);
+    }
+});
+
+
+/*
+// Direct Email sending without security
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'xxxxxxxxxx@gmail.com',
+        pass: 'xxxxxxxxxx'
+    }
+});
+
+var mailDetails = {
+    from: 'xxxxxxxxxx@gmail.com',
+    to: 'xxxxxxxxxx@yahoo.com',
+    subject: 'Using Node.js to send email',
+    text: 'Trying out new things bcos my God is able. Programming is sweet '
+};
+
+transporter.sendMail(mailDetails, function(err, data){
     if (err){
         console.log('Error, check your stuff');
     }
@@ -59,3 +57,4 @@ transporter.sendMail(mailOptions, function(err, data){
     }
 });
 */
+
