a187adfea09a30d4bafdd95ef17ad7e70227a463
Babajide Williams <babanila@yahoo.com>
2018-04-07 17:26:26 +0200

    k

--- NodeJS-Codes/readFile.js
+++ NodeJS-Codes/readFile.js
@@ -6,7 +6,7 @@ http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
 
-    fs.readFile(filename, function(err, data){
+    fs.readFile(__dirname + filename, function(err, data){
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end('Error !!! <br>  Not Found');
